3411e569f44db67e7fa0166c47ab79119abea702
Carlos Gonzalez <19charles96@gmail.com>
2016-10-25 19:24:37 +0100

    changed omp for to inner loop in collision ##

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -296,9 +296,11 @@ int collision(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obs
   ** NB the collision step is called after
   ** the propagate step and so values of interest
   ** are in the scratch-space grid */
-#pragma omp parallel for shared(w0,w1,w2,params,cells,tmp_cells,obstacles)
+#pragma omp parallel shared(w0,w1,w2,params,cells,tmp_cells,obstacles) private(ii)
+{
   for (int ii = 0; ii < params.ny; ii++)
   {
+#pragma omp for
     for (int jj = 0; jj < params.nx; jj++)
     {
       /* don't consider occupied cells */
@@ -363,6 +365,7 @@ int collision(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obs
       }
     }
   }
+}
   return EXIT_SUCCESS;
 }
 
