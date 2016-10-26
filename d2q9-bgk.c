bbea69a8be51a018da5724c8deabc591a8577a95
Carlos Gonzalez <19charles96@gmail.com>
2016-10-26 10:09:48 +0100

    trying to avoid thread creation again 3#

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -153,7 +153,7 @@ int main(int argc, char* argv[])
   /* iterate for maxIters timesteps */
   gettimeofday(&timstr, NULL);
   tic = timstr.tv_sec + (timstr.tv_usec / 1000000.0);
-#pragma omp parallel shared(params, cells, tmp_cells, obstacles,tot_cells,tot_u)
+#pragma omp parallel deafult(shared) shared(params, cells, tmp_cells, obstacles)
   for (int tt = 0; tt < params.maxIters; tt++)
   {
     timestep(params, cells, tmp_cells, obstacles);
